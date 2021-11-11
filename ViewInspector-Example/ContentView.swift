97304168f6017c2c81f9ae244ad18403da6449e2
William Boles <wibosco@users.noreply.github.com>
2021-11-11 09:28:31 +0000

    Initial project structure

---
+++ ViewInspector-Example/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  ViewInspector-Example
+//
+//  Created by William Boles on 11/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
---
+++ ViewInspector-Example/ViewInspector_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewInspector_ExampleApp.swift
+//  ViewInspector-Example
+//
+//  Created by William Boles on 11/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct ViewInspector_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
